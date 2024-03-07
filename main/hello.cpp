9bf2414de07ecae84ad9fe2af498c0b239ef64f4
Shreya-MB <149610846+Shreya-MB@users.noreply.github.com>
2024-03-07 15:50:27 +0530

    Webhook succesfully enabled

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World pple there\n";
+  cout << "Hello, World pple there and all\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
